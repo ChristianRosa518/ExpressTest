51499d87fba36083e761aba0656d76186f061f41
ChristianRosa518 <chrisrosa815518@gmail.com>
2023-05-31 21:43:40 -0400

    testy

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require("express");
-const app = express();
 require("dotenv").config();
+const home = require("./src/home");
 
 const port = process.env.PORT;
+
+const app = express();
+app.use(express.json());
+
+app.use("/home", home);
 
 app.get("/", (req, res) => res.send("hello world"));
 
